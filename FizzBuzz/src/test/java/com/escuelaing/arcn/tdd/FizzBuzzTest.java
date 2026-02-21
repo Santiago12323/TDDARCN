f13374353e73fe5cddcaeecff802b525fc4500b2
Santiago Coronado <156589422+Santiago12323@users.noreply.github.com>
2026-02-21 17:17:02 +0000

    pruebas de coverage

--- FizzBuzz/src/test/java/com/escuelaing/arcn/tdd/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/com/escuelaing/arcn/tdd/FizzBuzzTest.java
@@ -1,7 +1,10 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class FizzBuzzTest {
+
     @Test
     void testFizzBuzzReturnsNumber() {
         assertEquals("1", FizzBuzz.fizzbuzz(1));
@@ -21,6 +24,32 @@ class FizzBuzzTest {
     void testFizzBuzzMultiplo3y5() {
         assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
     }
-    
 
+    @Test
+    void testFizzBuzzZero() {
+        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(0));
+    }
+
+    @Test
+    void testFizzBuzzNegativeMultiplo3() {
+        assertEquals("Fizz", FizzBuzz.fizzbuzz(-3));
+    }
+
+    @Test
+    void testFizzBuzzNegativeMultiplo5() {
+        assertEquals("Buzz", FizzBuzz.fizzbuzz(-5));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "2, 2",
+        "4, 4",
+        "6, Fizz",
+        "10, Buzz",
+        "30, FizzBuzz",
+        "7, 7"
+    })
+    void testFizzBuzzMultipleValues(int input, String expected) {
+        assertEquals(expected, FizzBuzz.fizzbuzz(input));
+    }
 }
